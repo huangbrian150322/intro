9f9ba4c995b08fc7f0b889143635fc143cb525a5
Brian Huang <huangbrian15@gmail.com>
2020-10-27 16:21:42 -0400

    First commit

---
+++ intro.py
@@ -0,0 +1,16 @@
+# 1
+for num in range(2,6):
+    print("For loop on number", num)
+
+#2
+num = 0
+while True:
+    if num > 3:
+        break
+    print("While loop on number", num)
+    num += 1
+
+#3
+a = ['a', 'b', 'c', 'd', 'e', 'f']
+for letter in a:
+    print(letter)
